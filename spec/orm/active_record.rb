e61d262d650cb5e408b0bd823c47f751c91e7637
Peter Goldstein <peter.m.goldstein@gmail.com>
2018-04-23 17:11:15 -0700

    Update the migration code to accomodate changes in underlying ActiveRecord

--- spec/orm/active_record.rb
+++ spec/orm/active_record.rb
@@ -4,4 +4,8 @@
 ActiveRecord::Migration.verbose = false
 ActiveRecord::Base.logger = Logger.new(nil)
 
-ActiveRecord::Migrator.migrate(File.expand_path('../rails-app/db/migrate', __dir__))
+if ActiveRecord::Migrator.respond_to?(:migrate)
+  ActiveRecord::Migrator.migrate(File.expand_path('../rails-app/db/migrate', __dir__))
+else
+  ActiveRecord::MigrationContext.new(File.expand_path('../rails-app/db/migrate', __dir__)).migrate
+end
