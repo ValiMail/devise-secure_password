12234c930575085bc3ea8fd5cf2ec828828c6d62
Mark Eissler <mark.eissler@valimail.com>
2018-04-29 20:01:25 -0700

    [PLAT-484] Configure more reasonable defaults.
    The original defaults were based on best practices but were determiend to be overly secure but the follow on updates provided no security improvement whatsoever to a user's system and may actually have made such systems less secure. These new defaults are a compromise also based on best practices.

--- lib/generators/devise/templates/secure_password.rb
+++ lib/generators/devise/templates/secure_password.rb
@@ -10,34 +10,34 @@ Devise.setup do |config|
   # standard configuration parameter.
 
   # The number of uppercase letters (latin A-Z) required in a password:
-  # config.password_required_uppercase_count = 0
+  # config.password_required_uppercase_count = 1
 
   # The number of lowercase letters (latin A-Z) required in a password:
-  # config.password_required_lowercase_count = 0
+  # config.password_required_lowercase_count = 1
 
   # The number of numbers (0-9) required in a password:
-  # config.password_required_number_count = 0
+  # config.password_required_number_count = 1
 
   # The number of special characters (!@#$%^&*()_+-=[]{}|') required in a password:
-  # config.password_required_special_character_count = 0
+  # config.password_required_special_character_count = 1
 
   # ==> Configuration for the Devise Secure Password extension
   #     Module: password_disallows_frequent_reuse
   #
   # The number of previously used passwords that can not be reused:
-  # config.password_previously_used_count = 1
+  # config.password_previously_used_count = 8
 
   # ==> Configuration for the Devise Secure Password extension
   #     Module: password_disallows_frequent_changes
   #     *Requires* password_disallows_frequent_reuse
   #
   # The minimum time that must pass between password changes:
-  # config.password_minimum_age = 0.days
+  # config.password_minimum_age = 1.days
 
   # ==> Configuration for the Devise Secure Password extension
   #     Module: password_requires_regular_updates
   #     *Requires* password_disallows_frequent_reuse
   #
   # The maximum allowed age of a password:
-  # config.password_maximum_age = 365.days
+  # config.password_maximum_age = 180.days
 end
