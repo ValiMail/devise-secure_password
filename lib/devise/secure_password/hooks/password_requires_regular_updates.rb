d68b1ef6398b8d0a004b00d8ccad17987644ce2b
Mark Eissler <mark.eissler@valimail.com>
2018-05-17 12:32:59 -0700

    [PLATR-25] Fix for expired password not tracked when authy is configured.

--- lib/devise/secure_password/hooks/password_requires_regular_updates.rb
+++ lib/devise/secure_password/hooks/password_requires_regular_updates.rb
@@ -3,3 +3,9 @@ Warden::Manager.after_authentication do |user, warden, options|
     warden.session(options[:scope])[:secure_password_expired] = user.password_expired?
   end
 end
+
+Warden::Manager.after_fetch do |user, warden, options|
+  if user.respond_to?(:password_expired?) && !warden.session[:secure_password_expired]
+    warden.session(options[:scope])[:secure_password_expired] = user.password_expired?
+  end
+end
