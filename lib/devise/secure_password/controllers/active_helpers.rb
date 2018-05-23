4907b45d6c9b3883cf38c85e16882815623372d2
Dave Krupinski <david.krupinski@valimail.com>
2018-05-23 12:08:58 -0700

    Remove devise login flow alterations, password expiration checked via exposed controller method

--- lib/devise/secure_password/controllers/active_helpers.rb
+++ lib/devise/secure_password/controllers/active_helpers.rb
@@ -3,37 +3,26 @@ module Devise
     module Controllers
       module ActiveHelpers
         extend ActiveSupport::Concern
-
-        included do
-          before_action :pending_password_expired_redirect!, except: [:destroy]
+        def authenticate_secure_password_expired?
+          return if devise_controller?
+          return unless warden.authenticated?
+          return unless warden.session.present?
+          warden.session['secure_password_expired'] == true
         end
 
-        # Redirect to password change page if password needs to be changed.
-        def pending_password_expired_redirect!
-          return unless skip_current_controller? && redirected_in_session? && warden.session && warden.session['secure_password_expired']
-          redirect_to edit_user_password_with_policy_url, alert: "#{error_string_for_password_expired}."
+        def authenticate_secure_password!
+          return unless authenticate_secure_password_expired?
+          redirect_to authenticate_secure_password_path, alert: "#{error_string_for_password_expired}."
         end
 
-        def redirected_in_session?
-          warden.authenticated? && warden.session['secure_password_last_controller'] == 'Devise::SessionsController'
+        def authenticate_secure_password_path
+          scope = Devise::Mapping.find_scope!(warden.user)
+          :"edit_#{scope}_password_with_policy"
         end
 
-        # Prevent infinite loops and allow specified controllers to bypass.
-        # @NOTE: The ability to extend this list may be made public, in the
-        # future if that functionality is needed.
-        def skip_current_controller?
-          exclusion_list = [
-            'Devise::SessionsController',
-            'Devise::PasswordsWithPolicyController#edit',
-            'Devise::PasswordsWithPolicyController#update',
-            'DeviseInvitable::RegistrationsController#edit',
-            'DeviseInvitable::RegistrationsController#update'
-          ]
-          !(exclusion_list.include?("#{self.class.name}#" + action_name) || (exclusion_list & self.class.ancestors.map(&:to_s)).any?)
-        end
+        private
 
         def error_string_for_password_expired
-          return 'password expired' unless warden.user.class.respond_to?(:password_maximum_age)
           I18n.t(
             'secure_password.password_requires_regular_updates.errors.messages.password_expired',
             timeframe: distance_of_time_in_words(warden.user.class.password_maximum_age)
--- lib/devise/secure_password/hooks/password_requires_regular_updates.rb
+++ lib/devise/secure_password/hooks/password_requires_regular_updates.rb
@@ -1,5 +1,5 @@
-Warden::Manager.after_authentication do |user, warden, options|
-  if user.respond_to?(:password_expired?)
-    warden.session(options[:scope])[:secure_password_expired] = user.password_expired?
+Warden::Manager.after_authentication do |record, warden, options|
+  if record.respond_to?(:password_expired?)
+    warden.session(options[:scope])[:secure_password_expired] = record.password_expired?
   end
 end
