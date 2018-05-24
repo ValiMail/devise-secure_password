05ac73bc947940a4e9d6f6333fbbef366de81f33
Dave Krupinski <david.krupinski@valimail.com>
2018-05-24 00:03:59 -0700

    Ensure all warden keys are strings

--- lib/devise/secure_password/hooks/password_requires_regular_updates.rb
+++ lib/devise/secure_password/hooks/password_requires_regular_updates.rb
@@ -1,5 +1,5 @@
-Warden::Manager.after_authentication do |record, warden, options|
+Warden::Manager.after_set_user do |record, warden, options|
   if record.respond_to?(:password_expired?)
-    warden.session(options[:scope])[:secure_password_expired] = record.password_expired?
+    warden.session(options[:scope])['secure_password_expired'] = record.password_expired?
   end
 end
