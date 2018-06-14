efa9e26c8184e50a8d4386781e2bcb81e04b2b59
Mark Eissler <mark.eissler@valimail.com>
2018-06-14 16:29:54 -0700

    [PLAT-498] Fix formatting.

--- lib/devise/secure_password/grammar.rb
+++ lib/devise/secure_password/grammar.rb
@@ -6,7 +6,7 @@ module Devise
       # distance_in_words without determiner words: about, almost, over, etc.
       def precise_distance_of_time_in_words(from_time, to_time = 0, options = {})
         precise_options = { scope: :'secure_password.datetime.precise_distance_in_words' }
-        distance_of_time_in_words(from_time, to_time, options.merge!(precise_options) )
+        distance_of_time_in_words(from_time, to_time, options.merge!(precise_options))
       end
     end
   end
