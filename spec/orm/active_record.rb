3827fb75de69aa142d18ffe588b42799e8f3ae2c
Peter Goldstein <peter@valimail.com>
2020-11-09 09:33:34 -0800

    Spike on switching this gem entirely to Rails 6.
    Specs are running locally, model level are currently passing, still having some issues with asset packing

--- spec/orm/active_record.rb
+++ spec/orm/active_record.rb
@@ -3,9 +3,12 @@
 #
 ActiveRecord::Migration.verbose = false
 ActiveRecord::Base.logger = Logger.new(nil)
+path = File.expand_path('../rails-app/db/migrate', __dir__)
 
 if ActiveRecord::Migrator.respond_to?(:migrate)
-  ActiveRecord::Migrator.migrate(File.expand_path('../rails-app/db/migrate', __dir__))
+  ActiveRecord::Migrator.migrate(path)
 else
-  ActiveRecord::MigrationContext.new(File.expand_path('../rails-app/db/migrate', __dir__)).migrate
+  args = [path]
+  args << ActiveRecord::Base.connection.schema_migration if Gem.loaded_specs['rails'].version.to_s.match(/^6\.0/)
+  ActiveRecord::MigrationContext.new(*args).migrate
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 ENV['RAILS_ENV'] ||= 'test'
-ENV['RAILS_TARGET'] ||= '5.1'
+ENV['RAILS_TARGET'] ||= '6.0'
 
 #
 # Simplecov configuration (COVERAGE=true bundle exec rspec spec/)
