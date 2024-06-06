0ecfece1e3e4f762be7e36037bd49b713d506824
Tailor Fontela <taylorrf@gmail.com>
2024-06-06 16:59:38 -0300

    keep validates at model

--- lib/devise/secure_password/models/previous_password.rb
+++ lib/devise/secure_password/models/previous_password.rb
@@ -4,6 +4,7 @@ module Devise
       self.table_name = 'previous_passwords'
       belongs_to :user
       default_scope -> { order(id: :desc) }
+      validates :user_id, presence: true
       validates :salt, presence: true
       validates :encrypted_password, presence: true
 
