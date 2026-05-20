ef42d311c2c4d8d45a0fb8da1b39bc82292d1497
Tailor Fontela <taylorrf@gmail.com>
2026-05-20 17:27:47 -0300

    Support for Ruby 4.0 (#160)
    * support ruby 4.0
    
    * fix rubocop warning
    
    * fix js importmap local error
    
    * fix ruby 4.1 warning

--- spec/rails-app-7_0/config/application.rb
+++ spec/rails-app-7_0/config/application.rb
@@ -10,6 +10,7 @@ module RailsApp70
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
+    config.active_support.to_time_preserves_timezone = :zone
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
